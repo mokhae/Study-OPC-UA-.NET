e7356de5d13905bc67782c172bff968e962773e6
mokhae <mokhae70@gmail.com>
2022-04-05 10:19:38 +0900

    mokhae update

--- Step1-MinimalClientAndServer/MinimalClient/MinimalClient.Designer.cs
+++ Step1-MinimalClientAndServer/MinimalClient/MinimalClient.Designer.cs
@@ -30,16 +30,27 @@
         {
             this.ConnectButton = new System.Windows.Forms.Button();
             this.EndpointCB = new System.Windows.Forms.ComboBox();
-            this.browseTreeCtrl1 = new Opc.Ua.Sample.Controls.BrowseTreeCtrl();
+            this.txtNodeID = new System.Windows.Forms.TextBox();
+            this.ItemTree = new System.Windows.Forms.TreeView();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.btnAddList = new System.Windows.Forms.Button();
+            this.lstNodes = new System.Windows.Forms.ListBox();
+            this.btnRemoveList = new System.Windows.Forms.Button();
+            this.btnCreateSubs = new System.Windows.Forms.Button();
+            this.btnRemoveSubs = new System.Windows.Forms.Button();
+            this.txtSubs = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // ConnectButton
             // 
             this.ConnectButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.ConnectButton.Location = new System.Drawing.Point(384, 19);
-            this.ConnectButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.ConnectButton.Location = new System.Drawing.Point(336, 15);
+            this.ConnectButton.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.ConnectButton.Name = "ConnectButton";
-            this.ConnectButton.Size = new System.Drawing.Size(91, 26);
+            this.ConnectButton.Size = new System.Drawing.Size(80, 21);
             this.ConnectButton.TabIndex = 2;
             this.ConnectButton.Text = "Connect";
             this.ConnectButton.UseVisualStyleBackColor = true;
@@ -50,36 +61,149 @@
             this.EndpointCB.FormattingEnabled = true;
             this.EndpointCB.Items.AddRange(new object[] {
             "opc.tcp://localhost:51210/UA/MinimalServer",
-            "opc.tcp://localhost:51210/UA/SampleServer"});
-            this.EndpointCB.Location = new System.Drawing.Point(13, 19);
-            this.EndpointCB.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            "opc.tcp://localhost:51210/UA/SampleServer",
+            "opc.tcp://192.168.10.7"});
+            this.EndpointCB.Location = new System.Drawing.Point(11, 15);
+            this.EndpointCB.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.EndpointCB.Name = "EndpointCB";
-            this.EndpointCB.Size = new System.Drawing.Size(363, 23);
+            this.EndpointCB.Size = new System.Drawing.Size(318, 20);
             this.EndpointCB.TabIndex = 3;
             // 
-            // browseTreeCtrl1
+            // txtNodeID
             // 
-            this.browseTreeCtrl1.AttributesCtrl = null;
-            this.browseTreeCtrl1.EnableDragging = false;
-            this.browseTreeCtrl1.Location = new System.Drawing.Point(13, 62);
-            this.browseTreeCtrl1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.browseTreeCtrl1.Name = "browseTreeCtrl1";
-            this.browseTreeCtrl1.SessionTreeCtrl = null;
-            this.browseTreeCtrl1.Size = new System.Drawing.Size(462, 353);
-            this.browseTreeCtrl1.TabIndex = 4;
+            this.txtNodeID.Location = new System.Drawing.Point(432, 44);
+            this.txtNodeID.Multiline = true;
+            this.txtNodeID.Name = "txtNodeID";
+            this.txtNodeID.Size = new System.Drawing.Size(458, 41);
+            this.txtNodeID.TabIndex = 5;
+            // 
+            // ItemTree
+            // 
+            this.ItemTree.Location = new System.Drawing.Point(12, 41);
+            this.ItemTree.Name = "ItemTree";
+            this.ItemTree.Size = new System.Drawing.Size(403, 470);
+            this.ItemTree.TabIndex = 6;
+            this.ItemTree.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.ItemTree_AfterSelect);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(433, 141);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(96, 28);
+            this.btnRead.TabIndex = 7;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // txtValue
+            // 
+            this.txtValue.Location = new System.Drawing.Point(723, 109);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(167, 21);
+            this.txtValue.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(658, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Value : ";
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(535, 141);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(96, 28);
+            this.btnWrite.TabIndex = 10;
+            this.btnWrite.Text = "Write";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // btnAddList
+            // 
+            this.btnAddList.Location = new System.Drawing.Point(433, 180);
+            this.btnAddList.Name = "btnAddList";
+            this.btnAddList.Size = new System.Drawing.Size(156, 28);
+            this.btnAddList.TabIndex = 11;
+            this.btnAddList.Text = "Add List";
+            this.btnAddList.UseVisualStyleBackColor = true;
+            this.btnAddList.Click += new System.EventHandler(this.btnAddList_Click);
+            // 
+            // lstNodes
+            // 
+            this.lstNodes.FormattingEnabled = true;
+            this.lstNodes.ItemHeight = 12;
+            this.lstNodes.Location = new System.Drawing.Point(432, 214);
+            this.lstNodes.Name = "lstNodes";
+            this.lstNodes.Size = new System.Drawing.Size(459, 124);
+            this.lstNodes.TabIndex = 12;
+            // 
+            // btnRemoveList
+            // 
+            this.btnRemoveList.Location = new System.Drawing.Point(609, 180);
+            this.btnRemoveList.Name = "btnRemoveList";
+            this.btnRemoveList.Size = new System.Drawing.Size(156, 28);
+            this.btnRemoveList.TabIndex = 13;
+            this.btnRemoveList.Text = "Remove List";
+            this.btnRemoveList.UseVisualStyleBackColor = true;
+            this.btnRemoveList.Click += new System.EventHandler(this.btnRemoveList_Click);
+            // 
+            // btnCreateSubs
+            // 
+            this.btnCreateSubs.Location = new System.Drawing.Point(432, 346);
+            this.btnCreateSubs.Name = "btnCreateSubs";
+            this.btnCreateSubs.Size = new System.Drawing.Size(156, 28);
+            this.btnCreateSubs.TabIndex = 14;
+            this.btnCreateSubs.Text = "Create Subscription";
+            this.btnCreateSubs.UseVisualStyleBackColor = true;
+            this.btnCreateSubs.Click += new System.EventHandler(this.btnCreateSubs_Click);
+            // 
+            // btnRemoveSubs
+            // 
+            this.btnRemoveSubs.Location = new System.Drawing.Point(608, 346);
+            this.btnRemoveSubs.Name = "btnRemoveSubs";
+            this.btnRemoveSubs.Size = new System.Drawing.Size(156, 28);
+            this.btnRemoveSubs.TabIndex = 15;
+            this.btnRemoveSubs.Text = "Remove Subscription";
+            this.btnRemoveSubs.UseVisualStyleBackColor = true;
+            this.btnRemoveSubs.Click += new System.EventHandler(this.btnRemoveSubs_Click);
+            // 
+            // txtSubs
+            // 
+            this.txtSubs.Location = new System.Drawing.Point(432, 380);
+            this.txtSubs.Multiline = true;
+            this.txtSubs.Name = "txtSubs";
+            this.txtSubs.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtSubs.Size = new System.Drawing.Size(458, 131);
+            this.txtSubs.TabIndex = 16;
             // 
             // MinimalClient
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(511, 445);
-            this.Controls.Add(this.browseTreeCtrl1);
+            this.ClientSize = new System.Drawing.Size(902, 523);
+            this.Controls.Add(this.txtSubs);
+            this.Controls.Add(this.btnRemoveSubs);
+            this.Controls.Add(this.btnCreateSubs);
+            this.Controls.Add(this.btnRemoveList);
+            this.Controls.Add(this.lstNodes);
+            this.Controls.Add(this.btnAddList);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.ItemTree);
+            this.Controls.Add(this.txtNodeID);
             this.Controls.Add(this.EndpointCB);
             this.Controls.Add(this.ConnectButton);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "MinimalClient";
             this.Text = "MinimalClient";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MinimalClient_FormClosing);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -87,7 +211,18 @@
 
         private System.Windows.Forms.Button ConnectButton;
         private System.Windows.Forms.ComboBox EndpointCB;
-        private Opc.Ua.Sample.Controls.BrowseTreeCtrl browseTreeCtrl1;
+        private System.Windows.Forms.TextBox txtNodeID;
+        private System.Windows.Forms.TreeView ItemTree;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Button btnAddList;
+        private System.Windows.Forms.ListBox lstNodes;
+        private System.Windows.Forms.Button btnRemoveList;
+        private System.Windows.Forms.Button btnCreateSubs;
+        private System.Windows.Forms.Button btnRemoveSubs;
+        private System.Windows.Forms.TextBox txtSubs;
     }
 }
 
--- Step1-MinimalClientAndServer/MinimalClient/MinimalClient.cs
+++ Step1-MinimalClientAndServer/MinimalClient/MinimalClient.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.Collections;
 
 using Opc.Ua;
 using Opc.Ua.Configuration;
 using Opc.Ua.Client;
 using Opc.Ua.Sample.Controls;
-
+using System.Collections.Generic;
 
 namespace MinimalClient
 {
@@ -13,6 +14,11 @@ namespace MinimalClient
     {
         private ApplicationConfiguration _config;
         private Session _session;
+
+        private Subscription subscription;
+        private MonitoredItem monitoredItem;
+
+        private Hashtable oNode = new Hashtable();
 
         public MinimalClient()
         {
@@ -33,8 +39,15 @@ namespace MinimalClient
             _session = Session.Create(_config, new ConfiguredEndpoint(null, new EndpointDescription(endPointUrl)), true, "", 60000, null, null);
             
             //3 - Show the server namespace
-            browseTreeCtrl1.SetView(_session, BrowseViewType.Objects, null);
+            //browseTreeCtrl1.SetView(_session, BrowseViewType.Objects, null);
 
+            ReferenceDescriptionCollection refs;
+            byte[] continuationPoint;
+
+            _session.Browse(null, null, ObjectIds.ObjectsFolder, 0u, BrowseDirection.Forward, ReferenceTypeIds.HierarchicalReferences, true, (uint)NodeClass.Variable | (uint)NodeClass.Object | (uint)NodeClass.Method, out continuationPoint, out refs);
+
+
+            makeTree(refs);
 
             //BrowsTreeControl을 사용하지 않는 경우 아래 코드를 사용하여 AddressSpace를 순회할 수 있다.
 
@@ -53,10 +66,110 @@ namespace MinimalClient
             //    {
             //        Console.WriteLine("+ {0}: {1}, {2}", nextRd.DisplayName, nextRd.BrowseName, nextRd.NodeClass);
             //    }
-            //}                
+            //}
+
 
         }
-        
+
+        private void makeTree(ReferenceDescriptionCollection refs)
+        {
+            foreach (var rd in refs)
+            {
+                TreeNode item = new TreeNode(rd.DisplayName.ToString());
+                if (rd.NodeClass == NodeClass.Variable)
+                {
+                    item.Tag = rd;
+                }
+
+                Console.WriteLine("{0}: {1}, {2}", rd.DisplayName, rd.BrowseName, rd.NodeClass);
+
+
+                ReferenceDescriptionCollection nextRefs;
+                byte[] nextCp;
+                _session.Browse(null, null, ExpandedNodeId.ToNodeId(rd.NodeId, _session.NamespaceUris), 0u, BrowseDirection.Forward, ReferenceTypeIds.HierarchicalReferences, true, (uint)NodeClass.Variable | (uint)NodeClass.Object | (uint)NodeClass.Method, out nextCp, out nextRefs);
+
+                makeTreeChild(item, nextRefs);
+                //foreach (var nextRd in nextRefs)
+                //{
+                //    Console.WriteLine("+ {0}: {1}, {2}", nextRd.DisplayName, nextRd.BrowseName, nextRd.NodeClass);
+
+                //}
+
+                ItemTree.Nodes.Add(item);
+            }
+        }
+
+        private void makeTreeChild(TreeNode parent, ReferenceDescriptionCollection refs)
+        {
+            foreach (var rd in refs)
+            {
+                TreeNode item = new TreeNode(rd.DisplayName.ToString());
+                if (rd.NodeClass == NodeClass.Variable)
+                {
+                    item.Tag = rd;
+                }
+
+                Console.WriteLine("{0}: {1}, {2}", rd.DisplayName, rd.BrowseName, rd.NodeClass);
+
+
+                ReferenceDescriptionCollection nextRefs;
+                byte[] nextCp;
+                _session.Browse(null, null, ExpandedNodeId.ToNodeId(rd.NodeId, _session.NamespaceUris), 0u, BrowseDirection.Forward, ReferenceTypeIds.HierarchicalReferences, true, (uint)NodeClass.Variable | (uint)NodeClass.Object | (uint)NodeClass.Method, out nextCp, out nextRefs);
+
+                makeTreeChild(item, nextRefs);
+                //foreach (var nextRd in nextRefs)
+                //{
+                //    Console.WriteLine("+ {0}: {1}, {2}", nextRd.DisplayName, nextRd.BrowseName, nextRd.NodeClass);
+
+                //}
+
+                parent.Nodes.Add(item);
+            }
+        }
+        private void getNode(ReferenceDescriptionCollection refs)
+        {
+            foreach (var rd in refs)
+            {
+                Console.WriteLine("{0}: {1}, {2}", rd.DisplayName, rd.BrowseName, rd.NodeClass);
+                if (oNode.ContainsKey(rd.NodeId.ToString()) == false)
+                    oNode.Add(rd.NodeId.ToString(), rd);
+
+
+                ReferenceDescriptionCollection nextRefs;
+                byte[] nextCp;
+                _session.Browse(null, null, ExpandedNodeId.ToNodeId(rd.NodeId, _session.NamespaceUris), 0u, BrowseDirection.Forward, ReferenceTypeIds.HierarchicalReferences, true, (uint)NodeClass.Variable | (uint)NodeClass.Object | (uint)NodeClass.Method, out nextCp, out nextRefs);
+
+                getNodeR(nextRefs);
+                //foreach (var nextRd in nextRefs)
+                //{
+                //    Console.WriteLine("+ {0}: {1}, {2}", nextRd.DisplayName, nextRd.BrowseName, nextRd.NodeClass);
+
+                //}
+            }
+        }
+
+        private void getNodeR(ReferenceDescriptionCollection refs)
+        {
+            foreach (var rd in refs)
+            {
+                Console.WriteLine("{0}: {1}, {2}", rd.DisplayName, rd.BrowseName, rd.NodeClass);
+                if (oNode.ContainsKey(rd.NodeId.ToString()) == false)
+                    oNode.Add(rd.NodeId.ToString(), rd);
+
+
+                ReferenceDescriptionCollection nextRefs;
+                byte[] nextCp;
+                _session.Browse(null, null, ExpandedNodeId.ToNodeId(rd.NodeId, _session.NamespaceUris), 0u, BrowseDirection.Forward, ReferenceTypeIds.HierarchicalReferences, true, (uint)NodeClass.Variable | (uint)NodeClass.Object | (uint)NodeClass.Method, out nextCp, out nextRefs);
+
+                getNodeR(nextRefs);
+                //foreach (var nextRd in nextRefs)
+                //{
+                //    Console.WriteLine("+ {0}: {1}, {2}", nextRd.DisplayName, nextRd.BrowseName, nextRd.NodeClass);
+
+                //}
+            }
+        }
+
         private ApplicationConfiguration CreateOpcUaAppConfiguration()
         {
             var config = new ApplicationConfiguration()
@@ -98,5 +211,153 @@ namespace MinimalClient
                 _session = null;
             }            
         }
+
+
+        private void ItemTree_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            ReferenceDescription rd = (ReferenceDescription)e.Node.Tag;
+            if (rd.NodeClass == NodeClass.Variable)
+            {
+                txtNodeID.Text = rd.NodeId.ToString();
+                txtNodeID.Tag = rd;
+            }
+
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                NodeId nd = new NodeId(txtNodeID.Text);
+                txtValue.Text = ReadValue(nd).Value.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Read Error : " + ex.Message);
+            }
+        }
+
+        private DataValue ReadValue(NodeId nodeId)
+        {
+            return _session.ReadValue(nodeId);
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                NodeId nd = new NodeId(txtNodeID.Text);
+                ReferenceDescription rd = (ReferenceDescription)txtNodeID.Tag;
+                
+                writeValue(nd, txtValue.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Read Error : " + ex.Message);
+            }
+        }
+
+        private bool writeValue(NodeId nodeId, string writeValue)
+        {
+      
+
+            WriteValueCollection nodesToWrite = new WriteValueCollection();
+
+            WriteValue itemToWrite = new WriteValue();
+
+            itemToWrite.NodeId = nodeId;
+            itemToWrite.AttributeId = Attributes.Value;
+            
+            DataValue dv = new DataValue(Convert.ToInt16(writeValue));
+            
+            itemToWrite.Value = dv;
+
+
+            nodesToWrite.Add(itemToWrite);
+
+            StatusCodeCollection scc = null;
+            DiagnosticInfoCollection diagnosInfos = null;
+
+            ResponseHeader resp = _session.Write(
+                null, nodesToWrite, out scc, out diagnosInfos
+                );
+
+            if (StatusCode.IsBad(scc[0]))
+            {
+                ServiceResult result = ClientBase.GetResult(scc[0], 0, diagnosInfos, resp);
+                throw new ServiceResultException(result);
+                
+            }
+
+            return true;
+        }
+
+        private void btnAddList_Click(object sender, EventArgs e)
+        {
+            if (txtNodeID.Text.Length > 0)
+            {
+                if (lstNodes.Items.Contains(txtNodeID.Text) == false)
+                {
+
+                    lstNodes.Items.Add(txtNodeID.Text);
+                }
+            }
+        }
+
+        private void btnRemoveList_Click(object sender, EventArgs e)
+        {
+            if (lstNodes.SelectedItem != null)
+            {
+                lstNodes.Items.Remove(lstNodes.SelectedItem);
+            }
+        }
+
+        private void btnCreateSubs_Click(object sender, EventArgs e)
+        {
+            subscription = new Subscription(_session.DefaultSubscription)
+            {
+                PublishingEnabled = true,
+                PublishingInterval = 1000
+            };
+
+            var list = new List<MonitoredItem> { };
+            foreach (object itm in lstNodes.Items)
+            {
+                list.Add(new MonitoredItem(subscription.DefaultItem)
+                {
+                    DisplayName = itm.ToString(),
+                    StartNodeId = itm.ToString()
+                });
+            }
+            list.ForEach(i => i.Notification += new MonitoredItemNotificationEventHandler(OnNotification));
+            subscription.AddItems(list);
+
+            _session.AddSubscription(subscription);
+
+            subscription.Create();
+
+        }
+
+        private void OnNotification(MonitoredItem item, MonitoredItemNotificationEventArgs e)
+        {
+            MonitoredItemNotification notification = e.NotificationValue as MonitoredItemNotification;
+            foreach (var value in item.DequeueValues())
+            {
+                this.Invoke(new MethodInvoker(
+                    delegate ()
+                    {
+                        txtSubs.AppendText(String.Format("{0} : {1}\r\n", item.StartNodeId, value.Value.ToString()));
+                        txtSubs.ScrollToCaret();
+                    }));
+                
+            }
+        }
+
+        private void btnRemoveSubs_Click(object sender, EventArgs e)
+        {
+            subscription.Delete(true);
+        }
     }
+
+
 }
